48e63baec1b1cd1be7a316bf1593021a27503396
ds-sujin <124863799+ds-sujin@users.noreply.github.com>
2025-11-22 12:26:57 +0900

    Time: 179 ms (36.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+SELECT machine_id, ROUND(AVG(time)::numeric,3) AS processing_time
+FROM (
+    SELECT machine_id, process_id, MAX(case when activity_type = 'end' then timestamp end) -
+    MAX(case when activity_type = 'start' then timestamp end) AS time 
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS processtimetable
+GROUP BY machine_id
+
+
